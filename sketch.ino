aa41951e3df4e679c877008d8fe04923d6da8f45
Gonzalo0743 <64326722+Gonzalo0743@users.noreply.github.com>
2024-03-27 12:10:47 -0600

    Adding BCD logic and LCD prints

--- sketch.ino
+++ sketch.ino
@@ -1,4 +1,3 @@
-//Código Divisor de tensión con LDR.
 #include "LiquidCrystal_I2C.h"
 #include <Wire.h>
 
@@ -11,6 +10,9 @@ void setup(){
   pinMode(2, OUTPUT); // Salida digital para el primer bit de Gray
   pinMode(3, OUTPUT); // Salida digital para el segundo bit de Gray
   pinMode(4, OUTPUT); // Salida digital para el tercer bit de Gray 
+  pinMode(5, INPUT);  // Pin digital como entrada para el primer bit de Gray
+  pinMode(6, INPUT);  // Pin digital como entrada para el segundo bit de Gray
+  pinMode(7, INPUT);  // Pin digital como entrada para el tercer bit de Gray
   Serial.begin(9600);
   Wire.begin();
   lcd.init();
@@ -27,27 +29,52 @@ void loop(){
     Serial.println(codeGray);
     
     // Separar el código de Gray en bits individuales
-    int bit1 = codeGray.charAt(0) - '0';
-    int bit2 = codeGray.charAt(1) - '0';
-    int bit3 = codeGray.charAt(2) - '0';
+    int bit0 = codeGray.charAt(0) - '0';
+    int bit1 = codeGray.charAt(1) - '0';
+    int bit2 = codeGray.charAt(2) - '0';
     
     // Pasar cada bit por salidas digitales
-    digitalWrite(2, bit1);
-    digitalWrite(3, bit2);
-    digitalWrite(4, bit3);
+    digitalWrite(2, bit0);
+    digitalWrite(3, bit1);
+    digitalWrite(4, bit2);
+    
+    // Leer los bits de salida y mostrarlos en el puerto serie y en la pantalla LCD
+    int BitE0 = digitalRead(5);//C
+    int BitE1 = digitalRead(6);//B
+    int BitE2 = digitalRead(7);//A
+
+    //Pasar a de Exceso 3 circular a BCD
+    int BCD0 = (!BitE0);
+    int BCD1 = (!BitE1 && BitE0) || (BitE1 && !BitE0);
+    int BCD2 = (!BitE2 && !BitE1) || (!BitE2 && !BitE0) || (BitE2 && BitE1 && BitE0);
+
+    
+    // Mostrar los bits de salida en el puerto serie
+    /*Serial.print("Bits de salida leídos: ");
+    Serial.print(BitE2);
+    Serial.print(BitE1);
+    Serial.println(BitE0);*/
+    
+    // Mostrar los bits de salida en la pantalla LCD
+    lcd.setCursor(0, 0);
+    lcd.print("Exc3:");
+    lcd.print(BitE2);
+    lcd.print(BitE1);
+    lcd.print(BitE0);
+
+    lcd.setCursor(0,1);
+    lcd.print("BCD: ");
+    lcd.print(BCD2);
+    lcd.print(BCD1);
+    lcd.print(BCD0);
     
     delay(500);                
      
     delay(1000);
-    lcd.setCursor(0,1);
-
-    lcd.print(valueOut);
-    
-    delay(1000);
     lcd.clear(); 
-  }
+}
 
-//Funcion para pasar el decimal a codigo de gray
+// Función para pasar el decimal a código de Gray
 String toGray(int value) {
   // Implementación de la conversión a código Gray en formato binario
   int gray = (value >> 1) ^ value;
@@ -62,3 +89,4 @@ String toGray(int value) {
 
   return binary;
 }
+
